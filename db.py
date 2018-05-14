15ba1c57d0fc5f3840d1d632e1db8dbf4568bf25
yazenkova <37548243+yazenkova@users.noreply.github.com>
2018-05-14 17:51:27 +0300

    add comments

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
+# работа с бд
 import sqlite3
 
 
+# подсоединяемся к бд (если нет - создаем)
+# создаем таблицу,в которой будем хранить id чата и запомненный город
 def connect():
     conn = sqlite3.connect("mydatabase.db")
     cursor = conn.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS [user] (
-                    user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    row_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                     chat_id varchar(20) NOT NULL,
                     city varchar(20) NOT NULL,
                     date_and_time datetime NOT NULL);""")
@@ -13,6 +16,7 @@ def connect():
     return cursor, conn
 
 
+# функция добавления города для определенного чата в таблицу
 def add(chat_id, city):
     cursor, conn = connect()
     cursor.executemany('INSERT INTO user '
@@ -21,12 +25,14 @@ def add(chat_id, city):
     conn.commit()
 
 
+# удаляем строки, чтобы потом вставить новое значение города
 def remove_all_for_user(chat_id):
     cursor, conn = connect()
     cursor.execute('DELETE FROM user WHERE chat_id = ? ;', [(chat_id)])
     conn.commit()
 
 
+# достаем город для нужного чата из таблицы
 def get_cities(chat_id):
     cursor, conn = connect()
     cursor.execute('SELECT city FROM user WHERE chat_id = ?;', [(chat_id)])
