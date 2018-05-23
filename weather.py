2bf977b1dc8db1a72102c1a2047fb5bc692fb64b
yazenkova <37548243+yazenkova@users.noreply.github.com>
2018-05-24 02:41:33 +0300

    update

--- weather.py
+++ weather.py
@@ -1,40 +1,31 @@
 # работа с данными с сайта openweathermap.org
 # для удобства подключена библиотека pyowm
 # для построения графиков пользуемся matplotlib
+import os
 import pyowm
 import config
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 
-# сохраненние нужной информации
-def get(city):
-    owm = pyowm.OWM(API_key=config.api_key, language='en')
-    obs = owm.weather_at_place(city)
-    w = obs.get_weather()
-    fc = owm.three_hours_forecast(city)
-    f = fc.get_forecast()
-    return w, f, obs.get_location().get_country()
-
-
-# выделение нужной информации в словарь
-def format_weather(weather):
-    t = weather.get_temperature(unit='celsius')
-    ans = dict()
-    ans['Current temperature'] = t['temp']
-    ans['Maximum temperature'] = t['temp_max']
-    ans['Minimum temperature'] = t['temp_min']
-    ans['Status'] = weather.get_detailed_status()
-    ans['Sunrise'] = weather.get_sunrise_time('iso')
-    ans['Sunset'] = weather.get_sunset_time('iso')
-    ans['Humidity'] = weather.get_humidity()
-    ans['Pressure'] = int(weather.get_pressure()['press'] / 1.3332239)
-    ans['Wind'] = weather.get_wind()['speed']
-    return ans
+def short_information(weather):
+    weather_dict = OrderedDict()
+    if weather.get_temperature(unit='celsius')['temp'] > 0:
+        weather_dict['Temperature:'] = '+' + str(weather.get_temperature(unit='celsius')['temp'])
+    else:
+        weather_dict['Temperature:'] =str(weather.get_temperature(unit='celsius')['temp'])
+    weather_dict['Status:'] = str(weather.get_detailed_status())
+    weather_dict['Humidity:'] = str(weather.get_humidity()) + '%'
+    weather_dict['Pressure:'] = str(int(weather.get_pressure()['press'] / 1.3332239)) + ' mmhg'
+    weather_dict['Wind:'] = str(weather.get_wind()['speed']) + ' m/s'
+    return weather_dict
 
 
 # построение графика по данным о температуре на ближайшие 5 дней
 # сохраняем его как файл .png (после отправки сразу же удаляем)
 def make_image(forecast, city):
+    if not os.path.exists('images'):
+        os.makedirs('images')
     x_data = []
     y_data = []
     for weather in forecast:
@@ -47,27 +38,18 @@ def make_image(forecast, city):
     plt.plot(x_data, y_data)
     ax.set_title('Forecast in ' + city + ' for 5 days', size=25)
     ax.set_xlabel('date/time', size=20)
-    ax.set_ylabel('temperature in Celsium', size=20)
+    ax.set_ylabel('temperature in Celsius', size=20)
     plt.savefig('images/graph' + city + '.png', format='png')
 
 
-# оформляем обработанную информацию для ответа юзеру
-def info(city):
-    t_city = city
-    raw_w, raw_f, country = get(city)
-    w = format_weather(raw_w)
-    make_image(raw_f, t_city)
-    response = 'City: ' + str(t_city)
-    if ',' not in t_city:
-        response += ',' + country
-    response += '\n'
-    response += str(w['Status']) + '\n'
-    response += 'Temperature: '
-    if w['Current temperature'] > 0:
-        response += '+'
-    response += str(w['Current temperature']) + '\n'
-    response += 'Wind: ' + str(w['Wind']) + ' m/s\n'
-    response += 'Pressure: ' + str(w['Pressure']) + ' mmhg\n'
-    response += 'Sunrise: ' + str(w['Sunrise']) + '\n'
-    response += 'Sunset: ' + str(w['Sunset']) + '\n'
-    return response, str(w['Status'])
+def information(city):
+    owm = pyowm.OWM(API_key=config.api_key, language='en')
+    forecast = owm.three_hours_forecast(city)
+    f = forecast.get_forecast()
+    w = f.get_weathers()[0]
+    info = short_information(w)
+    response = 'City: ' + city + '\n'
+    for key in info.keys():
+        response += key + ' ' + info[key] + '\n'
+    make_image(f, city)
+    return response
